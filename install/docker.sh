e3057b6a160f2b48303212429fd67a90b357b92e
Nlp Trinh <torrances@yahoo.com>
2015-04-17 15:16:12 -0700

    update docker installation file to add modified docker-compose

--- install/docker.sh
+++ install/docker.sh
@@ -11,6 +11,8 @@ sudo apt-get update
 sudo apt-get install lxc-docker -y
 
 # install docker compose
-curl -L https://github.com/docker/compose/releases/download/1.1.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
+wget https://github.com/docker/compose/releases/download/1.1.0/docker-compose-$(uname -s)-$(uname -m)
+mv docker-compose-$(uname -s)-$(uname -m) docker-compose
+sudo chmod 777 docker-compose
+sudo mv docker-compose /usr/local/bin/
+docker-compose --version
