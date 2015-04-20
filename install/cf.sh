37c5fcffe6cb184d024fec9ab9b79c605eae9a7b
Craig Trim <craigtrim@gmail.com>
2015-04-19 22:32:40 -0700

    add subl install + fix bug in cf

--- install/cf.sh
+++ install/cf.sh
@@ -4,7 +4,7 @@
 export FILE=cf-cli_amd64.deb
 
 wget https://cli.run.pivotal.io/stable?release=debian64&version=6.11.0&source=github-rel
-mv stable?release=debian64 $FILE
+mv stable\?release\=debian64 $FILE
 sudo dpkg -i $FILE
 sudo apt-get install -f -y
 rm $FILE
---
+++ install/sublime3.sh
@@ -0,0 +1,10 @@
+# install sublime text 3
+# <http://askubuntu.com/questions/172698/how-do-i-install-sublime-text-2-3>
+
+sudo add-apt-repository \
+  ppa:webupd8team/sublime-text-3
+
+sudo apt-get update
+
+sudo apt-get install -y \
+  sublime-text-installer
