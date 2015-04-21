0092b42ef01332b85fc6c50a4cf60d33dbc05692
Craig Trim <craigtrim@gmail.com>
2015-04-21 15:46:12 -0700

    add ubuntu baseline script

---
+++ install/baseline-ubuntu.sh
@@ -0,0 +1,27 @@
+# remove libre office
+sudo apt-get \
+  remove --purge -y \
+    libreoffice* \
+    deja* \
+    mahjong* \
+    aisleriot* \
+    sudoku* \
+    mines* \
+    simple-scan* \
+    shotwell* \
+    unity-lens* \
+    empathy* \
+    gdrive* \
+    rhythymbox
+
+sudo apt-get \
+  clean
+
+sudo apt-get \
+  autoremove -y
+
+# boot-up utility manager
+# launch with "sudo bum"
+sudo apt-get \
+  install -y \
+    bum
