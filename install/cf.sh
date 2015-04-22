97af22b7321df7b58b8ace161daf64bda96300f6
Craig Trim <craigtrim@gmail.com>
2015-04-21 22:41:46 -0700

    improve headers

--- install/cf.sh
+++ install/cf.sh
@@ -1,8 +1,31 @@
+################################################################
+##
+## PURPOSE:
+## download and install CloudFoundry (CF)
+##
+## HISTORY:
+## 5:03 PM 4/20/2015 created 
+##
+## PREREQUSITES:
+## <none>
+##
+################################################################
 # Cloud Foundry command line interface
 # <https://github.com/cloudfoundry/cli/releases>
 
+sudo apt-get \
+  install -y \
+    wget
+
 export FILE=cf-cli_amd64.deb
-wget -O $FILE https://cli.run.pivotal.io/stable?release=debian64&version=6.11.0&source=github-rel
-sudo dpkg -i $FILE
-sudo apt-get install -f -y
+wget -O \
+  $FILE \
+    https://cli.run.pivotal.io/stable?release=debian64&version=6.11.0&source=github-rel
+
+sudo dpkg -i \
+  $FILE
+
+sudo apt-get \
+  install -f -y
+
 rm $FILE
--- install/ibm-ice.sh
+++ install/ibm-ice.sh
@@ -1,12 +1,29 @@
-# Before you can create a container in Bluemix, you must install the IBM Containers Extension.
-# <http://trimc-devops.blogspot.com/2015/03/configuring-python-development.html>
+################################################################
+##
+## PURPOSE:
+## download and build IBM ICE (IBM Container Extension)
+##
+## HISTORY:
+## 5:03 PM 4/20/2015 created 
+##
+## PREREQUSITES:
+## 1. docker > 1.4
+##
+## ATTRIBUTIONS:
+## prov:wasInfluencedBy <http://trimc-devops.blogspot.com/2015/03/configuring-python-development.html>
+##
+################################################################
 
 sudo apt-get \
   install -y \
     curl
 
 export ICE=icecli-2.0.zip
-
 curl -O https://static-ice.ng.bluemix.net/$ICE
-sudo pip install icecli-2.0.zip
+
+sudo pip \
+  install \
+    $ICE
+
 ice version
+rm $ICE
