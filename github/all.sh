c5b15ac47164bcd7cb43d54827626e2f870304fd
Craig Trim <cmtrim@us.ibm.com>
2015-05-03 14:45:29 -0700

    updat ebaseline scripts

--- github/all.sh
+++ github/all.sh
@@ -18,23 +18,26 @@
 ##
 ################################################################
 
-./pull.sh docker wsdoc
-sudo sh -c 'echo "\nexport WSDOC=/home/craig/workspaces/public/projects/docker" >> /etc/environment'
+./pull.sh docker WSPDO
+sudo sh -c 'echo "\nexport WSPDO=/home/craig/workspaces/public/projects/docker" >> /etc/environment'
 
-./pull.sh bom wsbom
-sudo sh -c 'echo "\nexport WSBOM=/home/craig/workspaces/public/projects/bom" >> /etc/environment'
+./pull.sh bom WSPBO
+sudo sh -c 'echo "\nexport WSPBO=/home/craig/workspaces/public/projects/bom" >> /etc/environment'
 
-./pull.sh java-commons wsjc
-sudo sh -c 'echo "\nexport WSJC=/home/craig/workspaces/public/projects/java-commons" >> /etc/environment'
+./pull.sh java-commons WSPJC
+sudo sh -c 'echo "\nexport WSPJC=/home/craig/workspaces/public/projects/java-commons" >> /etc/environment'
 
-./pull.sh snowball wssno
-sudo sh -c 'echo "\nexport WSSNO=/home/craig/workspaces/public/projects/snowball" >> /etc/environment'
+./pull.sh snowball WSPSN
+sudo sh -c 'echo "\nexport WSPSN=/home/craig/workspaces/public/projects/snowball" >> /etc/environment'
 
-./pull.sh solr-minimalist wssm
-sudo sh -c 'echo "\nexport WSSM=/home/craig/workspaces/public/projects/solr-minimalist" >> /etc/environment'
+./pull.sh solr-minimalist WSPSM
+sudo sh -c 'echo "\nexport WSPSM=/home/craig/workspaces/public/projects/solr-minimalist" >> /etc/environment'
 
-./pull.sh jaxrs-minimalist wsjm
-sudo sh -c 'echo "\nexport WSJM=/home/craig/workspaces/public/projects/jaxrs-minimalist" >> /etc/environment'
+./pull.sh jaxrs-minimalist WSPJM
+sudo sh -c 'echo "\nexport WSPJM=/home/craig/workspaces/public/projects/jaxrs-minimalist" >> /etc/environment'
 
-./pull.sh jaxrs-mongo wsjmg
-sudo sh -c 'echo "\nexport WSJMG=/home/craig/workspaces/public/projects/jaxrs-mongo" >> /etc/environment'
+./pull.sh jaxrs-mongo WSPJMO
+sudo sh -c 'echo "\nexport WSPJMO=/home/craig/workspaces/public/projects/jaxrs-mongo" >> /etc/environment'
+
+./pull.sh gngrams-scripts WSPGS
+sudo sh -c 'echo "\nexport WSPGS=/home/craig/workspaces/public/projects/jaxrs-mongo" >> /etc/environment'
--- install/baseline-ubuntu.sh
+++ install/baseline-ubuntu.sh
@@ -21,12 +21,18 @@ sudo apt-get \
 sudo apt-get \
   autoremove -y
 
-# boot-up utility manager
-# launch with "sudo bum"
+# overview
+# bum, boot-up utility manager, launch with "sudo bum"
+# glances, system usage overview window, launch with "glances"
+# tree, ascii tree output of directory at any location on terminal, launch with "tree"
+# libgnome2-bin, open a gnome window from any location on terminal, launch with "gnome-open ."
 sudo apt-get \
   install -y \
     bum \
-    glances
+    glances \
+    tree \
+    libgnome2-bin
 
 # update .bashrc
 sh -c 'echo "\nsource /etc/environment" >> ~/.bashrc'
+sh -c 'echo "\nalias opn=\"gnone-open .\"" >> ~/.bashrc'
